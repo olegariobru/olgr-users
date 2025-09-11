86733e4fa1230f22715f5cc3f95ed623df14c4f6
olegariobru <b_olgr@icloud.com>
2025-09-11 16:33:04 -0300

    tela de loggin estilizada

--- frontEND/src/paginas/login/index.js
+++ frontEND/src/paginas/login/index.js
@@ -1,12 +1,30 @@
 import styles from './login.module.css'
-
+import { motion } from 'framer-motion'
 
 export default function LoginPrin (){
     return(
-        <div className={styles.lgHeader}>
-            <div className={styles.lgHeader2}>
-                <p>olg&lt;Users&gt;.</p>
+        <motion.div
+            initial={{ opacity: 0, y: 20}}
+            animate={{ opacity: 1, y: 0}}
+            exit={{ opacity: 0, y: -20}}
+            transition={{ duration: 0.5}}
+        ><div>
+            <div className={styles.lgHeader}>
+                <h1>olg&lt;Users&gt;.</h1>
+            
+            </div>
+
+            <div className={styles.lgBody}>
+                <h1 className={styles.lgBodyTitle}>Login</h1>
+                <form className={styles.lgForm}>
+                    <label className={styles.lgLabel} htmlFor="email">Email</label>
+                    <input className={styles.lgInput} type="email" id="email" name="email" required />
+                    <label className={styles.lgLabel} htmlFor="password">Senha</label>
+                    <input className={styles.lgInput} type="password" id="password" name="password" required />
+                    <button className={styles.lgButton} type="submit">Entrar</button>
+                </form>
             </div>
         </div>
+        </motion.div>
     )
 }
