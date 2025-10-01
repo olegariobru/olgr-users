2d083413ac3ee9b11b26b2a42493eb5fe2f81cbf
olegariobru <b_olgr@icloud.com>
2025-10-01 14:01:43 -0300

    ajuste na tela de cadastro e inputs

--- frontEND/src/paginas/cadastro/index.js
+++ frontEND/src/paginas/cadastro/index.js
@@ -3,6 +3,7 @@ import styles from './cadastro.module.css';
 import { useState } from 'react';
 import { auth } from '../../firebaseConfig';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Cadastro() {
   const [form, setForm] = useState({
@@ -13,18 +14,43 @@ export default function Cadastro() {
   });
 
   const [mensagem, setMensagem] = useState('');
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
+  const validarEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+  const validarSenha = (senha) => {
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/;
+    return regex.test(senha);
+  };
+
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setMensagem('');
 
+    if (!validarEmail(form.email)) {
+      setMensagem('❌ O email informado não é válido.');
+      return;
+    }
+    if (!validarSenha(form.senha)) {
+      setMensagem('❌ A senha deve ter pelo menos 6 caracteres, incluindo letras, números e caracteres especiais.');
+      return;
+    }
+
     try {
       await createUserWithEmailAndPassword(auth, form.email, form.senha);
       setMensagem('✅ Usuário cadastrado com sucesso!');
+
+      setTimeout(() => {
+        navigate('/login');
+      }, 2000);
+
     } catch (error) {
       if (error.code === 'auth/email-already-in-use') {
         setMensagem('❌ Este email já está em uso.');
@@ -40,46 +66,46 @@ export default function Cadastro() {
 
   return (
     <motion.div
-      className={styles.container}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.5 }}
     >
-      <h2>Crie sua conta</h2>
-      <p>Vamos começar com suas informações básicas</p>
+      <div className={styles.cadBody}>
+        <form className={styles.cadForm} onSubmit={handleSubmit}>
 
-      {mensagem && <p>{mensagem}</p>}
+          <div className={styles.cadFormDiv}>
+            <p className={styles.cadFormTitle}>Crie sua conta</p>
+            <p className={styles.cadFormSubtitle}>Preencha suas informações abaixo.</p>
+          </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="nome"
-          placeholder="Nome"
-          value={form.nome}
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={form.email}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="senha"
-          placeholder="Senha"
-          value={form.senha}
-          onChange={handleChange}
-        />
-        <input
-          type="tel"
-          name="telefone"
-          placeholder="Telefone"
-          value={form.telefone}
-          onChange={handleChange}
-        />
-        <button type="submit">Cadastrar</button>
-      </form>
+          <label className={styles.cadLabel} htmlFor="nome">Nome</label>
+          <input className={styles.inputCad} type="text" id="nome" name="nome" value={form.nome} onChange={handleChange} />
+
+          <label className={styles.cadLabel} htmlFor="email">Email</label>
+          <input className={styles.inputCad} type="text" id="email" name="email" value={form.email} onChange={handleChange} />
+
+          <label className={styles.cadLabel} htmlFor="senha">Senha</label>
+          <input className={styles.inputCad} type="password" id="senha" name="senha" value={form.senha} onChange={handleChange} />
+
+
+          <button className={styles.btnCad} type="submit">Cadastrar</button>
+
+          {mensagem && (
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+              className={mensagem.startsWith("✅") ? styles.msgSucesso : styles.msgErro}
+            >
+              {mensagem}
+            </motion.p>
+          )}
+
+          <p className={styles.cadFormP}>Já tem conta? <Link to="/login" className={styles.cadLink}>Entrar</Link></p>
+        </form>
+      </div>
     </motion.div>
   );
 }
